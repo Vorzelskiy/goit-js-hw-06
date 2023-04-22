29094cdd35370b572fbfda7158fb303fd03cdfbb
Vorzelskiy <evgeniysashko@gmail.com>
2023-04-22 11:53:46 +0300

    creaet task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ul = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  ul.appendChild(li);
+});
+
