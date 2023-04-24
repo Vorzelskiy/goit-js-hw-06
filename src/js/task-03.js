678f15135d8d9d632f139bc2fa94fe3a254594bf
Vorzelskiy <evgeniysashko@gmail.com>
2023-04-24 12:40:30 +0300

    creat task-03

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,15 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const gallery = document.querySelector('.gallery');
+
+const galleryItems = images.map(image => {
+  return `
+    <li>
+      <img src="${image.url}" alt="${image.alt}">
+    </li>
+  `;
+}).join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryItems);
