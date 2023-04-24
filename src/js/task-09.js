dd3f96add906abaf9587cddad7f735bbd576967c
Vorzelskiy <evgeniysashko@gmail.com>
2023-04-24 13:17:29 +0300

    creat task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,12 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+  const button = document.querySelector('.change-color');
+  const colorSpan = document.querySelector('.color');
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+  button.addEventListener('click', () => {
+    const newColor = getRandomHexColor();
+    document.body.style.backgroundColor = newColor;
+    colorSpan.textContent = newColor;
+  });
