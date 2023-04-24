67e1c9bc9e0719e90db5b67874dbcc44043dd9d9
Vorzelskiy <evgeniysashko@gmail.com>
2023-04-24 13:13:30 +0300

    creat task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector('.login-form');
 
+  loginForm.addEventListener('submit', (event) => {
+    event.preventDefault(); 
+
+    const emailInput = loginForm.elements.email;
+    const passwordInput = loginForm.elements.password;
+
+    if (!emailInput.value || !passwordInput.value) {
+      alert('Будь ласка, заповніть всі поля форми');
+      return;
+    }
+
+     const formData = {
+      email: emailInput.value,
+      password: passwordInput.value,
+    };
+
+    console.log(formData);
+
+    loginForm.reset(); 
+  });
