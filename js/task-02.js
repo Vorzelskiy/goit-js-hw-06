b8f1a1e7a908d3b790de2f9c9ab9a322ac86c249
Vorzelskiy <evgeniysashko@gmail.com>
2023-04-24 15:06:05 +0300

    fix task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,18 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
 
 const ul = document.querySelector("#ingredients");
 
-ingredients.forEach((ingredient) => {
+const ingredientListItems = ingredients.map((ingredient) => {
   const li = document.createElement("li");
   li.textContent = ingredient;
   li.classList.add("item");
-  ul.appendChild(li);
+  return li;
 });
+
+ul.append(...ingredientListItems);
 
